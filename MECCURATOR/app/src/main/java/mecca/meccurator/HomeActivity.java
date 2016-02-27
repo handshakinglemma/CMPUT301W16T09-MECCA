426c2d42004d2c49d631d966199b30374afa0bb9
Mercy Woldmariam <mercywoldmariam@uws110380usr.uws.ualberta.ca>
2016-02-27 15:53:53 -0700

    push

--- MECCURATOR/app/src/main/java/mecca/meccurator/HomeActivity.java
+++ MECCURATOR/app/src/main/java/mecca/meccurator/HomeActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,19 +21,18 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        Button viewArtListingButton = (Button) findViewById(R.id.viewMyArtListings);
+
+        viewArtListingButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+
+                Intent intent = new Intent(HomeActivity.this, ViewArtListingsActivity.class);
+                startActivity(intent);
             }
         });
 
-        public void viewMyListings(View view) {
-            Intent intent = new Intent(this, ViewArtListingsActivity.class);
-            startActivity(intent);
-    }
+
     }
 
 
