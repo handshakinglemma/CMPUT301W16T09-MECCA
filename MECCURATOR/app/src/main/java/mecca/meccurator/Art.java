9f75b8fdd65bb4acbc85a4bf7234e64659b6b9b4
Chaitali Patel <chaitali@uws111288usr.uws.ualberta.ca>
2016-02-27 12:02:54 -0700

    new Art class

---
+++ MECCURATOR/app/src/main/java/mecca/meccurator/Art.java
@@ -0,0 +1,22 @@
+package mecca.meccurator;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+
+/**
+ * Created by chaitali on 16-02-27.
+ */
+public class Art {
+
+    private String status;
+    private String owner;
+    private String borrower;
+    private String description;
+    private String artist;
+    private String title;
+    private Blob photo;
+    private String dimensions;
+    private BigDecimal minprice;
+
+
+}
