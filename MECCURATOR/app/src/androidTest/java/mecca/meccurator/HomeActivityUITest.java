5c82c0438fda332df741f912d7b0caf12bb9eacf
Colleen Venhuis <venhuis@ualberta.ca>
2016-02-27 13:14:10 -0700

    Added more test cases

---
+++ MECCURATOR/app/src/androidTest/java/mecca/meccurator/HomeActivityUITest.java
@@ -0,0 +1,12 @@
+package mecca.meccurator;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by cjvenhuis on 2016-02-27.
+ */
+public class HomeActivityUITest extends ActivityInstrumentationTestCase2 {
+    public HomeActivityUITest() {
+        super(HomeActivity.class);
+    }
+}
---
+++ MECCURATOR/app/src/androidTest/java/mecca/meccurator/testBid.java
@@ -0,0 +1,12 @@
+package mecca.meccurator;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by cjvenhuis on 2016-02-27.
+ */
+public class testBid extends ActivityInstrumentationTestCase2{
+    public testBid() {
+        super(HomeActivity.class);
+    }
+}
