54dcfc27824fe9549523574e6903daf64f1ba193
Alanna McLafferty <mclaffer@ualberta.ca>
2016-02-27 12:38:28 -0700

    Added ViewMyListingsActivity

---
+++ MECCURATOR/app/src/main/java/mecca/meccurator/ViewMyListingsActivity.java
@@ -0,0 +1,16 @@
+package mecca.meccurator;
+
+/**
+ * Created by svetlanna on 16-02-27.
+ */
+public class ViewMyListingsActivity {
+    private ArtList itemlistinglist;
+
+    public ArtList getItemlistinglist() {
+        return itemlistinglist;
+    }
+
+    public void setItemlistinglist(ArtList itemlistinglist) {
+        this.itemlistinglist = itemlistinglist;
+    }
+}
