f4523e8206715d265feedff14fa4828f1dc0e502
Chaitali Patel <chaitali@chaitalis-mbp.gateway.2wire.net>
2016-02-27 20:00:00 -0700

    added button and intent

--- MECCURATOR/app/src/main/java/mecca/meccurator/HomeActivity.java
+++ MECCURATOR/app/src/main/java/mecca/meccurator/HomeActivity.java
@@ -17,24 +17,12 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    //    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    //    setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    //    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    //    fab.setOnClickListener(new View.OnClickListener() {
-    //        @Override
-    //        public void onClick(View view) {
-    //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-    //                    .setAction("Action", null).show();
-    //        }
-    //    });
-
-    //public void viewMyListings(View view) {
-    //        Intent intent = new Intent(this, ViewArtListingsActivity.class);
-    //        startActivity(intent);
-    //    }
 
     }
+
 
 
     @Override
@@ -57,5 +45,12 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    // click to view art listings
+    public void ViewListingsButton(View view) {
+        Intent intent = new Intent(this, ViewArtListingsActivity.class);
+        startActivity(intent);
     }
 }
