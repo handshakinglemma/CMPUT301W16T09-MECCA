e4b5be6cb258c84b63d318a60bddfd957c18287c
Mercy Woldmariam <mercywoldmariam@Mercys-MacBook-Pro.local>
2016-03-01 00:35:05 -0700

    This is a new commit for what I originally planned to be an amendmend

--- MECCURATOR/app/src/main/java/mecca/meccurator/HomeActivity.java
+++ MECCURATOR/app/src/main/java/mecca/meccurator/HomeActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class HomeActivity extends AppCompatActivity {
 
