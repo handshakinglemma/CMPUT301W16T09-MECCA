055babfb10777726a4dc605ce1dbe4644f5c3593
Mercy Woldmariam <mercywoldmariam@Mercys-MacBook-Pro.local>
2016-03-13 14:11:38 -0600

    Fixed the my listings

--- MECCURATOR/app/src/main/java/mecca/meccurator/ViewMyListingsActivity.java
+++ MECCURATOR/app/src/main/java/mecca/meccurator/ViewMyListingsActivity.java
@@ -81,28 +81,32 @@ public class ViewMyListingsActivity extends AppCompatActivity implements OnItemS
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String choiceSelected = parent.getItemAtPosition(position).toString().split(" ")[0];
+                selectedArt = new ArrayList<Art>();
 
                 if (choiceSelected.equals("All")) {
-                    adapter = new ArrayAdapter<Art>(ViewMyListingsActivity.this,
-                            R.layout.list_item, ArtList.allArt);
-                    oldArtListings.setAdapter(adapter);
-                    adapter.notifyDataSetChanged();
-
-
-                } else {
-                    selectedArt = new ArrayList<Art>();
-
-                    for (Art a : ArtList.allArt) {
-                        if (a.getStatus().toLowerCase().trim().equals(choiceSelected.toLowerCase().trim())) {
+                    for (Art a: ArtList.allArt) {
+                        if (a.getOwner().toLowerCase().trim().equals(current_user.toLowerCase().trim())) {
                             selectedArt.add(a);
                         }
                     }
 
-                    adapter = new ArrayAdapter<Art>(ViewMyListingsActivity.this,
-                            R.layout.list_item, selectedArt);
-                    oldArtListings.setAdapter(adapter);
-                    adapter.notifyDataSetChanged();
+
+                } else {
+
+                    for (Art a : ArtList.allArt) {
+                        if (a.getOwner().toLowerCase().trim().equals(current_user.toLowerCase().trim())) {
+                            if (a.getStatus().toLowerCase().trim().equals(choiceSelected.toLowerCase().trim())) {
+                                selectedArt.add(a);
+                            }
+                        }
+
+                    }
                 }
+
+                adapter = new ArrayAdapter<Art>(ViewMyListingsActivity.this, R.layout.list_item, selectedArt);
+                oldArtListings.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+                Toast.makeText(parent.getContext(), "Selected: " + choiceSelected, Toast.LENGTH_LONG).show();
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
