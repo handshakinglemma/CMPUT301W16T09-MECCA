8e79c394094a4e449bbaaa31ed3b61bf88c6c0b0
Colleen Venhuis <venhuis@ualberta.ca>
2016-03-14 15:57:42 -0600

    more test cases!

--- MECCURATOR/app/src/androidTest/java/mecca/meccurator/testUser.java
+++ MECCURATOR/app/src/androidTest/java/mecca/meccurator/testUser.java
@@ -11,85 +11,34 @@ public class testUser extends ActivityInstrumentationTestCase2 {
     }
 
     public void testGetUsername() {
-        /*User user = new User("coolperson", "Emma", "7801234567", "emma@sunshine.com");
+        User user = new User("coolperson", "emma@sunshine.com");
         assertEquals("coolperson", user.getUsername());
 
         String returned = user.getUsername();
-        assertEquals(returned, user.getUsername());*/
+        assertEquals(returned, user.getUsername());
     }
 
     public void testSetUsername() {
-        /*User user = new User("coolperson", "Emma", "7801234567", "emma@sunshine.com");
+        User user = new User("coolperson", "emma@sunshine.com");
         assertEquals("coolperson", user.getUsername());
 
         user.setUsername("awesomeperson");
-        assertEquals("awesomeperson", user.getUsername());*/
-    }
-
-    public void testGetName() {
-        /*User user = new User("coolperson", "Emma", "7801234567", "emma@sunshine.com");
-        assertEquals("Emma", user.getName());
-
-        String returned = user.getName();
-        assertEquals(returned, user.getName());*/
-    }
-
-    public void testSetName() {
-        /*User user = new User("coolperson", "Emma", "7801234567", "emma@sunshine.com");
-        assertEquals("Emma", user.getName());
-
-        user.setName("McDonald");
-        assertEquals("McDonald", user.getName());*/
-    }
-
-    public void testGetPhoneNumber() {
-        /*User user = new User("coolperson", "Emma", "7801234567", "emma@sunshine.com");
-        assertEquals("7801234567", user.getPhoneNumber());
-
-        String returned = user.getPhoneNumber();
-        assertEquals(returned, user.getPhoneNumber());*/
-    }
-
-    public void testSetPhoneNumber() {
-        /*User user = new User("coolperson", "Emma", "7801234567", "emma@sunshine.com");
-        assertEquals("7801234567", user.getPhoneNumber());
-
-        user.setPhoneNumber("7807654321");
-        assertEquals("7807654321", user.getPhoneNumber());*/
-    }
-
-    public void testGetPhoto() {
-        /*User user = new User("coolperson", "Emma", "7801234567", "emma@sunshine.com");
-        Blob photo = new Blob();
-        assertEquals(photo, user.getPhoto());
-
-        String returned = user.getEmail();
-        assertEquals(returned, user.getEmail());*/
-    }
-
-    public void testSetPhoto() {
-        /*User user = new User("coolperson", "Emma", "7801234567", "emma@sunshine.com");
-        Blob photo = new Blob();
-        assertEquals(photo, user.getPhoto());
-
-        Blob picture = new Blob();
-        user.setPhoto(picture);
-        assertEquals(picture, user.getPhoto());*/
+        assertEquals("awesomeperson", user.getUsername());
     }
 
     public void testGetEmail() {
-        /*User user = new User("coolperson", "Emma", "7801234567", "emma@sunshine.com");
+        User user = new User("coolperson", "emma@sunshine.com");
         assertEquals("emma@sunshine.com", user.getEmail());
 
         String returned = user.getEmail();
-        assertEquals(returned, user.getEmail());*/
+        assertEquals(returned, user.getEmail());
     }
 
     public void testSetEmail() {
-        /*User user = new User("coolperson", "Emma", "7801234567", "emma@sunshine.com");
+        User user = new User("coolperson", "emma@sunshine.com");
         assertEquals("emma@sunshine.com", user.getEmail());
 
         user.setEmail("mcdonald@sunshine.com");
-        assertEquals("mcdonald@sunshine.com", user.getEmail());*/
+        assertEquals("mcdonald@sunshine.com", user.getEmail());
     }
 }
--- MECCURATOR/app/src/androidTest/java/mecca/meccurator/testUserList.java
+++ MECCURATOR/app/src/androidTest/java/mecca/meccurator/testUserList.java
@@ -2,6 +2,8 @@ package mecca.meccurator;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import java.util.ArrayList;
+
 /**
  * Created by cjvenhuis on 2016-02-27.
  */
@@ -10,7 +12,48 @@ public class testUserList extends ActivityInstrumentationTestCase2 {
         super(HomeActivity.class);
     }
 
-    public void testGetSize() {}
+    public void testAddUser() {
+        UserList users = new UserList();
+        User user = new User("emma", "emma@ualberta.ca");
+        // add user and assert that it's been added
+        users.addUser(user);
+        assertTrue(users.hasUser(user));
+    }
 
-    public void testSetSize() {}
+    public void testHasUser() {
+        UserList users = new UserList();
+        User user = new User("emma", "emma@ualberta.ca");
+        // assert that it has no users
+        assertFalse(users.hasUser(user));
+        // add a user
+        users.addUser(user);
+        // assert that it has user in it
+        assertTrue(users.hasUser(user));
+    }
+
+    public void testUpdateUser() {
+        // Nothing implemented to test
+    }
+
+    public void testDeleteUser() {
+        UserList users = new UserList();
+        User user = new User("emma", "emma@ualberta.ca");
+        users.addUser(user);
+        assertTrue(users.hasUser(user));
+
+        users.deleteUser(user);
+        assertFalse(users.hasUser(user));
+    }
+
+    public void testGetUsers() {
+        UserList users = new UserList();
+        User user = new User("emma", "emma@ualebrta.ca");
+        users.addUser(user);
+
+        ArrayList<User> returned = new ArrayList();
+        returned.add(user);
+        assertEquals(returned, users.getUsers());
+    }
+
+    public void testGetSize() {}
 }
