f7bc0488c0faceea7f021b7828c953afc33bc43f
Mercy Woldmariam <mercywoldmariam@uws110613usr.uws.ualberta.ca>
2016-03-24 12:55:14 -0600

    Working on UI test cases

--- MECCURATOR/app/src/androidTest/java/mecca/meccurator/HomeActivityUITest.java
+++ MECCURATOR/app/src/androidTest/java/mecca/meccurator/HomeActivityUITest.java
@@ -1,13 +1,41 @@
 package mecca.meccurator;
 
+import android.support.test.espresso.ViewAssertion;
 import android.test.ActivityInstrumentationTestCase2;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ViewAsserts;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * UI tests for Home Activity.
  * Created by cjvenhuis on 2016-02-27.
  */
-public class HomeActivityUITest extends ActivityInstrumentationTestCase2 {
-    public HomeActivityUITest() {
-        super(HomeActivity.class);
-    }
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class HomeActivityUITest {
+
+    @Rule
+    public final ActivityTestRule<HomeActivity> homeActivityActivityTestRule =
+            new ActivityTestRule<>(HomeActivity.class);
+
+    /*@Test
+    public void checkHomeDescription() {
+        onView(withId(R.id.homeDescription))
+                .check((matches(withText("Hello"))));
+    }*/
 }
