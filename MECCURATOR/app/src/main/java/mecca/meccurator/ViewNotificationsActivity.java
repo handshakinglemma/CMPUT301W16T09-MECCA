7448f925f5201984a9265ddfb4cf848ef2d2f536
Chaitali Patel <cpatel1@ualberta.ca>
2016-04-02 22:48:48 -0600

    added watchlist activity

--- MECCURATOR/app/src/main/java/mecca/meccurator/ViewNotificationsActivity.java
+++ MECCURATOR/app/src/main/java/mecca/meccurator/ViewNotificationsActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -89,6 +90,22 @@ public class ViewNotificationsActivity extends AppCompatActivity {
         UserList.users.remove(pos);
 
         UserList.users.add(pos, newestUser);
+
+        Button viewWatchList = (Button) findViewById(R.id.watchList);
+        viewWatchList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewWatchList(v);
+
+            }
+        });
+
+
+    }
+
+    private void viewWatchList(View v) {
+        Intent intent = new Intent(this, ViewWatchListActivity.class);
+        startActivity(intent);
     }
 
     @Override
