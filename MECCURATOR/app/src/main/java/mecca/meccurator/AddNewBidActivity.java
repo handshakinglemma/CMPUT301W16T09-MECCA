95a6a89d33239cb7bd98730a77dbad770d2185cb
Colleen Venhuis <venhuis@ualberta.ca>
2016-04-03 17:20:43 -0600

    rearranged some things to see if it caused the bug, but I can't replicate the bug...

--- MECCURATOR/app/src/main/java/mecca/meccurator/AddNewBidActivity.java
+++ MECCURATOR/app/src/main/java/mecca/meccurator/AddNewBidActivity.java
@@ -54,13 +54,25 @@ public class AddNewBidActivity extends AppCompatActivity {
         current_user = newbid.getStringExtra("current_user");
         owner = newbid.getStringExtra("owner");
 
+        ElasticsearchUserController.GetUserListTask getUserListTask = new ElasticsearchUserController.GetUserListTask();
+        getUserListTask.execute();
+
+        try {
+            userList = new ArrayList<User>();
+            userList.addAll(getUserListTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
         ownerpos = 0;
 
-        for(User user: UserList.users){
+        for(User user: userList){
             if (owner.equals(user.getUsername())){
                 break;
             }
-            ownerpos++;
+            ++ownerpos;
         }
 
         Button userProfile = (Button) findViewById(R.id.view_owner);
@@ -93,7 +105,9 @@ public class AddNewBidActivity extends AppCompatActivity {
             Toast.makeText(context, saved, duration).show();
             finish();
         }
+
         loadValues();
+
     }
 
     private void loadValues() {
@@ -106,7 +120,8 @@ public class AddNewBidActivity extends AppCompatActivity {
         TextView inputLengthDimensions = (TextView) findViewById(R.id.enterLengthDimensions);
         TextView inputWidthDimensions = (TextView) findViewById(R.id.enterWidthDimensions);
         TextView inputOwner = (TextView) findViewById(R.id.owner);
-        
+
+
         /* append data into EditText box */
         Art art = ArtList.allArt.get(pos);
         inputArtist.setText(art.getArtist());
